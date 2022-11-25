7bd9c0243c53b1ca13bfcf6bbd8e8a7631ba718d
Tomás Navarro Cabrera <i.themrwolf.i@gmail.com>
2022-11-25 23:59:45 +0000

    Add gh-pages, vite-plugin-pwa and custom paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Add Custom Paths for imports
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// Add PWA functionality
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(), // Add Custom Paths for imports
+    VitePWA({ registerType: 'autoUpdate' }), // Add PWA functionality
+  ],
 })
