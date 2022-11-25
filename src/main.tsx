186bb6570c5e38cc561e6f26002d9787037f907e
Tomás Navarro Cabrera <i.themrwolf.i@gmail.com>
2022-11-25 23:58:05 +0000

    Add normalize CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+// Add CSS to render all elements more consistenly in all browsers
+import './normalize.css'
+
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
